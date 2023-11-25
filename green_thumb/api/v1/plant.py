ddfcf1764da0928bd93d618b358b01ad8c752165
dthelegend <me@daudi.dev>
2023-11-25 23:05:12 +0000

    API works?

--- green_thumb/api/v1/plant.py
+++ green_thumb/api/v1/plant.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
 import green_thumb.db as db
 import green_thumb.db.models as models
 import green_thumb.db.schemas as schemas
@@ -16,7 +17,6 @@ async def read_plant_list(database: Session = Depends(db.get_db)) -> list[schema
 async def read_plant_by_id(plant_id: int, database: Session = Depends(db.get_db)) -> schemas.Plant:
     plant = database.query(models.Plant).get(plant_id)
 
-    print("hi")
     if plant is None:
         raise HTTPException(status_code=404, detail="Plant not found")
 
@@ -24,25 +24,27 @@ async def read_plant_by_id(plant_id: int, database: Session = Depends(db.get_db)
 
 @router.post("")
 async def create_plant(plant: schemas.PlantCreate, database: Session = Depends(db.get_db)) -> schemas.Plant:
-    database.add(**plant)
+    plant_m = models.Plant(**plant.model_dump())
+    try:
+        database.add(plant_m)
+    except IntegrityError as e:
+        raise HTTPException(status_code=401, detail=e.detail)
+
     database.commit()
-    database.refresh(plant)
-    return plant
+    database.refresh(plant_m)
+    return plant_m
 
 @router.post("/{plant_id}")
 async def update_plant_by_id(plant_id: int, plant: schemas.PlantUpdate, database: Session = Depends(db.get_db)) -> schemas.Plant:
     if plant.id != plant_id:
         raise HTTPException(status_code=401, detail="Plant ids in request do not match")
 
-    plant_fetched = database.query(models.Plant).get(plant_id)
+    plant_fetched = database.query(models.Plant).where(models.Plant.id == plant_id)
 
-    if plant_fetched is None:
+    rows_updated = plant_fetched.update(plant.model_dump())
+    if rows_updated != 1:
         raise HTTPException(status_code=404, detail="Plant not found")
-    
-    plant_fetched.update(**plant)
 
-    database.add(plant_fetched)
     database.commit()
-    database.refresh(plant_fetched)
 
-    return plant_fetched
+    return database.query(models.Plant).get(plant_id)
