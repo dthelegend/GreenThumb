0458ef6ed95729bfd982c7849dc89c05e403aab7
dthelegend <me@daudi.dev>
2023-11-26 11:02:09 +0000

    Fix where timeout called

--- green_thumb/lorax/emotion.py
+++ green_thumb/lorax/emotion.py
@@ -4,12 +4,13 @@ import json
 SERIAL_PATH = '/dev/serial/by-id/usb-Arduino__www.arduino.cc__0042_95036303235351909121-if00'
 
 ser = serial.Serial(SERIAL_PATH)
+ser.timeout = None
 
 def send_message(sev: int, text: str):
     ser.write(str.encode(sev) + text + '\0')
 
 def recv_message():
-    b = ser.read_until(b'\x00', timeout=None)
+    b = ser.read_until(b'\x00')
 
     try:
         return json.loads(b)
